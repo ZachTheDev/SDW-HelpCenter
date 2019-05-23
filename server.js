cee2170d9fb38e38f5db17622b950f97b780c543
Zach <zach@kodebalt.com>
2019-05-23 11:57:15 -0500

    cleanup n things

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-const express = require('express')
-const app = express()
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/public';
 
+app.use("/assets", express.static(__dirname + '/assets'));
 
+app.post("/add-tasks", function (req, res) {
+    fs.write("tasks.json", "JSON", req.body);
+});
 
-app.get('/', function (req, res) {
-    res.render('index')
-})
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+
+router.get("/writeArticle", function (req, res) {
+    res.sendFile(path + "articleEditor.html");
+});
+
+app.use("/", router);
+
+app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
+});
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+    console.log("Live at Port 3000");
+});
